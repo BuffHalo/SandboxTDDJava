d669b92ca9ffb6d9236dae7fc8a70ce2c409dc3d
BuffHalo <2.pi.richard@gmail.com>
2022-04-05 17:20:50 +0200

    Added Tennis Score Kata

--- src/main/java/tdd/sandbox/com/example/tdddemo/TennisCourtScore.java
+++ src/main/java/tdd/sandbox/com/example/tdddemo/TennisCourtScore.java
@@ -1,5 +1,9 @@
 package tdd.sandbox.com.example.tdddemo;
 
 public class TennisCourtScore {
+
+    public static String tennisToString(String string) {
+        return null;
+    }
     
 }
--- src/test/java/tdd/sandbox/com/example/tdddemo/TennisCourtScoreTest.java
+++ src/test/java/tdd/sandbox/com/example/tdddemo/TennisCourtScoreTest.java
@@ -1,5 +1,12 @@
 package tdd.sandbox.com.example.tdddemo;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TennisCourtScoreTest {
-    
+
+    @Test
+    public void two(){
+    Assert.assertEquals("00", "love", TennisCourtScore.tennisToString("love"));
+    }
 }
