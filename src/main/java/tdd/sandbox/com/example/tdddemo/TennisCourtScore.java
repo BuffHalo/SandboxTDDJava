1d57d5f9030819e3bf405110ece2ee836764dc6d
BuffHalo <2.pi.richard@gmail.com>
2022-04-07 17:40:41 +0200

    TDD | Adding TennisScore practice class + test

--- src/main/java/tdd/sandbox/com/example/tdddemo/TennisCourtScore.java
+++ src/main/java/tdd/sandbox/com/example/tdddemo/TennisCourtScore.java
@@ -1,9 +1,56 @@
 package tdd.sandbox.com.example.tdddemo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+
 //TODO added comment for commit
 public class TennisCourtScore {
 
-    public static String tennisToString(String string) {
-        return null;
+    public static String getScore(String scoreDeTennis) {
+        if (scoreDeTennis == "0-0") {
+            return "Love All";
+        } else {
+            return createTennisScore(scoreTennisToString(scoreDeTennis));
+        }
+
     }
-    
+
+    private static List<Character> scoreTennisToString(String scoreAsString) {
+
+        List<Character> chars = new ArrayList<>();
+
+        for (char ch : scoreAsString.toCharArray()) {
+            chars.add(ch);
+        }
+        return chars;
+    }
+
+    private final static String createTennisScore(List<Character> chars) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < chars.size(); i++) {
+            Character actual = chars.get(i);
+            result.append(translateScore(actual.toString()));
+        }
+
+        return result.toString();
+    }
+
+    private static String translateScore(String string) {
+        switch (string) {
+            case "3":
+                return "40";
+            case "2":
+                return "30";
+            case "1":
+                return "15";
+            case "0":
+                return "0";
+            case "-":
+                return "-";
+        }
+        throw new IllegalArgumentException("Invalid score: " + string);
+    }
+
 }
--- src/test/java/tdd/sandbox/com/example/tdddemo/TennisCourtScoreTest.java
+++ src/test/java/tdd/sandbox/com/example/tdddemo/TennisCourtScoreTest.java
@@ -3,10 +3,13 @@ package tdd.sandbox.com.example.tdddemo;
 import org.junit.Assert;
 import org.junit.Test;
 //TODO added comment for commit
+
 public class TennisCourtScoreTest {
 
     @Test
     public void two(){
-    Assert.assertEquals("00", "love", TennisCourtScore.tennisToString("love"));
+    Assert.assertEquals("0-0", "Love All", TennisCourtScore.getScore("0-0"));
+    Assert.assertEquals("1-0", "15-0", TennisCourtScore.getScore("1-0"));
+    Assert.assertEquals("1-1", "15-15", TennisCourtScore.getScore("1-1"));
     }
 }
